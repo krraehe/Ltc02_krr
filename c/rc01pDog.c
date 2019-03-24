4749c050cb636916595312ca71d1d8a79f36ff84
krraehe <krraehe@gmail.com>
2019-03-24 21:07:50 +0200

    recursion 01 dead dogs poem

---
+++ c/rc01pDog.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+
+//just a cruel endless poem about a priest end his dogs, in russian =(
+    
+int Upopa()
+{
+    printf ("у попа была собака, он её любил\n oна съела кусок мяса - он её убил!\n в землю закопал,\n и надпись написал, что ");
+    Upopa();
+}
+
+int main ()
+{
+   Upopa();
+}
